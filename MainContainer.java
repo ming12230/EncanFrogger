cc250be38c33be04868bc8e684493bd327140c8a
minchunie <jjparcero@up.edu.ph>
2026-04-08 16:48:38 +0800

    panels are gamelauncher then gamesecondpage

--- MainContainer.java
+++ MainContainer.java
@@ -1,55 +1,58 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class MainContainer extends JFrame{
-    
+public class MainContainer extends JFrame {
 
     JPanel mainPanel;
     CardLayout cardLayout;
 
     GameLauncher gameLaunch;
+    GameSecondPage secondPage;
     GamePanel gamePanel;
 
     public MainContainer(){
         setTitle("EncanFrogger");
-        //set icon/logo here po
-        setSize(850, 500); //to be edited
+        setSize(850, 500);
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        gameLaunch = new GameLauncher();
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        
+        gameLaunch = new GameLauncher(this);
+        secondPage = new GameSecondPage(this);
         gamePanel = new GamePanel();
 
-
-        cardLayout = new CardLayout();
-        mainPanel = new JPanel (cardLayout);
-
-        mainPanel.add(gameLaunch, "Launch Game");
-        mainPanel.add(gamePanel, "Game Start");
+        mainPanel.add(gameLaunch, "Launch");
+        mainPanel.add(secondPage, "SecondPage");
+        mainPanel.add(gamePanel, "Game");
 
         add(mainPanel);
-        
+
         launchGame();
-        
+
         setVisible(true);
     }
 
     public void launchGame(){
-        cardLayout.show(mainPanel, "Launch Game");
+        cardLayout.show(mainPanel, "Launch");
+    }
+
+    public void showSecondPage(){
+        cardLayout.show(mainPanel, "SecondPage");
     }
 
     public void startGame(){
-        cardLayout.show(mainPanel, "Start Game");
+        cardLayout.show(mainPanel, "Game");
     }
 
     public void menuGame(){
-        cardLayout.show(mainPanel, "Launch Game");//to be edited once mayda na menu class
+        cardLayout.show(mainPanel, "SecondPage"); 
     }
 
-    public static void main (String[] args){
+    public static void main(String[] args){
         new MainContainer();
     }
 }
-
-
